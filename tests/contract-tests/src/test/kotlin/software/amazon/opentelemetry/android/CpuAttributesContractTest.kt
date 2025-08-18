f4c6e17dd601401bd796a224d13f060a07aced7c
Leonardo Serrano <dhlserrano@gmail.com>
2025-08-18 12:57:36 -0700

    fix linting

--- tests/contract-tests/src/test/kotlin/software/amazon/opentelemetry/android/CpuAttributesContractTest.kt
+++ tests/contract-tests/src/test/kotlin/software/amazon/opentelemetry/android/CpuAttributesContractTest.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package software.amazon.opentelemetry.android
 
 import org.junit.jupiter.api.Assertions
@@ -12,14 +26,15 @@ class CpuAttributesContractTest {
 
     @Test
     fun `Spans with duration should have cpu utilization attributes`(data: ParsedOtlpData) {
-        val appSpans = data.traces.spans().filter {
-            it.attributes.has("service.name", APP_NAME)
-        }
+        val appSpans =
+            data.traces.spans().filter {
+                it.attributes.has("service.name", APP_NAME)
+            }
 
         Assertions.assertTrue(
             appSpans.all { span ->
                 !span.hasDuration() || span.attributes.has("process.cpu.avg_utilization")
-            }
+            },
         )
     }
 }
