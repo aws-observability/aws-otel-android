240e8eecbdf79d33112fa1817b9e5adb1cc9e833
sasirekha3 <ksasirekha@gmail.com>
2025-10-24 16:06:34 -0700

    chore: create release workflows to publish repo to Maven Central

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("adot.android-library")
     id("adot.android-publish")
+    id("signing")
+    id("maven-publish")
 }
 
 android {
@@ -27,6 +29,46 @@ dependencies {
     api(libs.opentelemetry.android.httpurlconnection)
     api(libs.opentelemetry.android.okhttp3)
     implementation(project(":ui-loading"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name.set("aws-otel-android")
+                description.set("AWS Distro for OpenTelemetry (ADOT) on Android")
+                url.set("https://github.com/aws-observability/aws-otel-android")
+                inceptionYear.set("2025")
+                scm {
+                    url.set("https://github.com/aws-observability/aws-otel-android/tree/main")
+                    connection.set("scm:git:ssh://git@github.com/aws-observability/aws-otel-android.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/aws-observability/aws-otel-android.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven{
+            url = uri("https://aws.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.findProperty("sonatypeUsername") as String?
+                password = project.findProperty("sonatypePassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
 
 tasks.withType<Test> {
