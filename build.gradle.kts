975c195893640550b40c3f05605180cfc592e37a
sasirekha3 <ksasirekha@gmail.com>
2025-11-13 16:50:46 -0800

    fix: update workflows and gradle publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,11 @@ buildscript {
 
 plugins {
     id("adot.spotless")
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    alias(libs.plugins.publishPlugin)
 }
 
 extra["java_version"] = JavaVersion.VERSION_1_8
 extra["kotlin_min_supported_version"] = KotlinVersion.KOTLIN_1_8
-val postReleaseTask = tasks.named("release")
 
 allprojects {
     repositories {
@@ -30,83 +29,6 @@ allprojects {
     }
     if (findProperty("final") != "true") {
         version = "$version-beta"
-    }
-
-    plugins.withId("maven-publish") {
-        plugins.apply("signing")
-
-        afterEvaluate {
-            val publishTask = tasks.named("publishToSonatype")
-
-            postReleaseTask.configure {
-                dependsOn(publishTask)
-            }
-        }
-
-        configure<PublishingExtension> {
-            publications {
-                register<MavenPublication>("maven") {
-                    afterEvaluate {
-                        artifactId = project.findProperty("archivesBaseName") as String
-                    }
-
-                    plugins.withId("java-platform") {
-                        from(components["javaPlatform"])
-                    }
-                    plugins.withId("java") {
-                        from(components["java"])
-                    }
-
-                    versionMapping {
-                        allVariants {
-                            fromResolutionResult()
-                        }
-                    }
-
-                    pom {
-                        name.set("AWS Distro for OpenTelemetry Android SDK")
-                        description.set(
-                            "The Amazon Web Services SDK of the OpenTelemetry Android Instrumentation.",
-                        )
-                        url.set("https:/github.com/aws-observability/aws-otel-android")
-
-                        licenses {
-                            license {
-                                name.set("Apache License, Version 2.0")
-                                url.set("https://aws.amazon.com/apache2.0")
-                                distribution.set("repo")
-                            }
-                        }
-
-                        developers {
-                            developer {
-                                id.set("amazonwebservices")
-                                organization.set("Amazon Web Services")
-                                organizationUrl.set("https://aws.amazon.com")
-                                roles.add("developer")
-                            }
-                        }
-
-                        scm {
-                            connection.set("scm:git:git@github.com:aws-observability/aws-otel-android.git")
-                            developerConnection.set("scm:git:git@github.com:aws-observability/aws-otel-android.git")
-                            url.set("https://github.com/aws-observability/aws-otel-android.git")
-                        }
-                    }
-                }
-            }
-        }
-
-        tasks.withType<Sign>().configureEach {
-            onlyIf { System.getenv("CI") == "true" }
-        }
-
-        configure<SigningExtension> {
-            val signingKey = System.getenv("GPG_PRIVATE_KEY")
-            val signingPassword = System.getenv("GPG_PASSPHRASE")
-            useInMemoryPgpKeys(signingKey, signingPassword)
-            sign(the<PublishingExtension>().publications["maven"])
-        }
     }
 }
 
